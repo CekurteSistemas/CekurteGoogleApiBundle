ecf0ad62842ac726c85239368ebdb1939a0db3e8
João Paulo Cercal <sistemas@cekurte.com>
2014-06-27 11:18:26 -0300

    Dependency Injection configuration.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Cekurte\GoogleApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('cekurte_google_api');
+
+        $rootNode
+            ->children()
+                ->scalarNode('cekurte_google_api_application_name')->end()
+                ->scalarNode('cekurte_google_api_client_id')->end()
+                ->scalarNode('cekurte_google_api_client_secret')->end()
+                ->scalarNode('cekurte_google_api_redirect_uri')->end()
+                ->scalarNode('cekurte_google_api_developer_key')->end()
+                ->scalarNode('cekurte_google_api_use_objects')->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
